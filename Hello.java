036cfb5e661010ff536f6d6d90ac8996883ca575
sourceTree <sourcetree@mail.com>
2024-01-05 22:48:44 -0700

    23 Functionarity

--- Hello.java
+++ Hello.java
@@ -1,27 +1,24 @@
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.function.Supplier;
 
 public class Hello{
     public static void main(String[] args) {
-        Function<Integer, Integer> myFunc = x -> x * 2;
-        myFunc.apply(7);
+        BiFunction<Integer, Integer, Integer> myFunc = (num1, num2) -> num1 * num2;
+        myFunc.apply(7,9);
 
-        Function<Integer, String> myFunc2 = num -> "Value is " + num;
-        myFunc2.apply(7);
+        BiFunction<Integer, Integer, String> myFunc2 = (num1, num2) -> "Value is " + (num1 + num2);
+        myFunc2.apply(7,9);
 
-        Consumer<String> myCons = name -> System.out.println("Hello " + name);
-        myCons.accept("Koushik");
+        BiConsumer<Integer, String> myCons = (num, name) -> System.out.println("Hello " + num + " " + name);
+        myCons.accept(1, "Koushik");
 
+        //there is no BiSupplier
         Supplier<Double> mySupp = () -> Math.random();
         mySupp.get();
 
-        Predicate<Integer> myPred = num -> (num % 2) == 0;
-        myPred.test(7);
-
-        //this is not part of function package.
-        Runnable myRunn = () -> System.out.println();
-        myRunn.run();
+        BiPredicate<Integer, Integer> myPred = (num1, num2) -> (num1 % 2) == num2;
+        myPred.test(7, 9);
     }
 }
