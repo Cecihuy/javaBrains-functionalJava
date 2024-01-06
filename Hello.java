5a3ba27a11279eaafe50a4ddf80a1eb358d0dc61
sourceTree <sourcetree@mail.com>
2024-01-06 09:29:06 -0700

    45 Distinct And Sorted Operators

--- Hello.java
+++ Hello.java
@@ -2,10 +2,9 @@ import java.util.stream.Stream;
 
 public class Hello{
     public static void main(String[] args){
-        Stream<String> names = Stream.of("apple", "banana", "mango", "strawberry");
-        names.map(String::toUpperCase)
-            .peek(str -> System.out.println("Peek result : " + str))
-            .filter(name -> name.length() < 6)
+        Stream<Integer> numbers = Stream.of(1,2,9,8,3,4,5,6,7,8,9,10);
+        numbers.distinct()
+            .sorted()
             .forEach(System.out::println);
     }
 }
