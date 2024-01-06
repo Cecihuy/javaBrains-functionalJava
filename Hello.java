9b53880996bb9cdace081bf2247e903c31178471
sourceTree <sourcetree@mail.com>
2024-01-05 23:31:39 -0700

    26 Method Reference Examples

--- Hello.java
+++ Hello.java
@@ -1,12 +1,30 @@
-import java.util.function.BinaryOperator;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
-
+/*
+ * compiler can figure out what input arg is
+ */
 public class Hello{
     public static void main(String[] args) {
-        UnaryOperator<Integer> myFunc = num -> num * 9;
-        myFunc.apply(7);
+        //lambda without method reference
+        Supplier<Double> mySupp = () -> Math.random();                
+        //with method reference
+        Supplier<Double> mySuppReff = Math::random;
 
-        BinaryOperator<String> myFunc2 = (num1, num2) -> "Value is " + num1 + num2;
-        myFunc2.apply("Java", "Koushik");
+        //lambda without method reference
+        Consumer<String> myCons = name -> System.out.println(name);
+        //with method reference
+        Consumer<String> myConsReff = System.out::println;
+
+        //lambda without method reference
+        UnaryOperator<String> myUnaryOp = str -> str.trim();
+        //with method reference
+        UnaryOperator<String> myUnaryOpReff = String::trim;
+
+        //lambda without method reference
+        BiPredicate<String, String> myBinaryPred = (a, b) -> a.equalsIgnoreCase(b);
+        //with method reference
+        BiPredicate<String, String> myBinaryPredReff = String::equalsIgnoreCase;
     }
 }
