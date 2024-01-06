f0d21f6905f862221df95549c6065169f4f92e21
sourceTree <sourcetree@mail.com>
2024-01-06 09:45:32 -0700

    47 Working With Object Streams

--- Hello.java
+++ Hello.java
@@ -1,11 +1,39 @@
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Hello{
     public static void main(String[] args){
-        Stream<Integer> oddNumbers = Stream.of(1,3,5,7,9);
-        Stream<Integer> evenNumbers = Stream.of(0,2,4,6,8);
+        Stream<Student> studenStream = Stream.of(
+            new Student(1, "Foo"),
+            new Student(2, "Bar"),
+            new Student(3, "A")
+        );
+        List<String> studentNames = studenStream
+            .filter(s -> s.getName().length() > 2)
+            .map(s -> s.getName())
+            .toList();
+        System.out.println("Student Names = " + studentNames);
+    }
+}
+class Student{
+    int id;
+    String name;   
 
-        Stream.concat(evenNumbers, oddNumbers)
-            .forEach(System.out::println);
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
