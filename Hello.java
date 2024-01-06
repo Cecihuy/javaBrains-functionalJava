0e42829656899ba52fe5269389aef16eef25f33b
sourceTree <sourcetree@mail.com>
2024-01-06 09:02:28 -0700

    42 The Map Operator

--- Hello.java
+++ Hello.java
@@ -2,7 +2,9 @@ import java.util.stream.Stream;
 
 public class Hello{
     public static void main(String[] args){
-        Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        numbers.filter(num -> num %2 == 0).limit(2).forEach(System.out::println);
+        Stream<String> names = Stream.of("apple", "banana", "mango", "strawberry");
+        names.limit(2)
+            .map(String::toUpperCase)            
+            .forEach(System.out::println);
     }
 }
