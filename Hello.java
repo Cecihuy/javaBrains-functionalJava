1f27ccebd448fbcc808994cb91103895f03f3927
sourceTree <sourcetree@mail.com>
2024-01-06 09:12:24 -0700

    43 The Peek Operator And Debugging

--- Hello.java
+++ Hello.java
@@ -3,8 +3,9 @@ import java.util.stream.Stream;
 public class Hello{
     public static void main(String[] args){
         Stream<String> names = Stream.of("apple", "banana", "mango", "strawberry");
-        names.limit(2)
-            .map(String::toUpperCase)            
+        names.map(String::toUpperCase)
+            .peek(str -> System.out.println("Peek result : " + str))
+            .filter(name -> name.length() < 6)
             .forEach(System.out::println);
     }
 }
