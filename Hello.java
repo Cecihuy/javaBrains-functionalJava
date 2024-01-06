7723a508eae8055163a636e144edd7a2efef9ef8
sourceTree <sourcetree@mail.com>
2024-01-06 08:53:45 -0700

    41 Limit And Filter Operators

--- Hello.java
+++ Hello.java
@@ -1,12 +1,8 @@
-import java.util.List;
 import java.util.stream.Stream;
 
 public class Hello{
     public static void main(String[] args){
-        List<Integer> upToFive = Stream.iterate(1, num -> num + 3)
-            .limit(5)
-            .toList();
-        
-        System.out.println(upToFive);            
+        Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        numbers.filter(num -> num %2 == 0).limit(2).forEach(System.out::println);
     }
 }
